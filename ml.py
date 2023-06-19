fbbd0ef78c3cb3a6110ecc1d21b98fa00ec3bf94
Fenil Daxeshkumar Mavani <62711537+FenilMavani@users.noreply.github.com>
2023-06-19 15:31:58 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -1,75 +1,100 @@
 import streamlit as st
 import yfinance as yf
-import pandas as pd
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM
+import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv1D, MaxPooling1D, Flatten
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
-def main():
-    st.title('Stock Market Prediction')
-    st.write('Enter the stock symbol and select the prediction period:')
-    
-    symbol = st.text_input('Stock Symbol (e.g., AAPL for Apple):')
-    period = st.selectbox('Prediction Period', ['1d', '5d', '1mo', '3mo'])
-    
-    if st.button('Predict'):
-        st.write('Fetching stock data...')
-        df = yf.download(symbol, period=period)
-        df = df[['Close']]
-        
-        st.write('Preprocessing data...')
-        dataset = df.values
-        dataset = dataset.astype('float32')
-        
-        train_size = int(len(dataset) * 0.8)
-        train_data, test_data = dataset[:train_size], dataset[train_size:]
-        
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        train_data = scaler.fit_transform(train_data)
-        
-        def create_dataset(data, lookback):
-            dataX, dataY = [], []
-            for i in range(len(data) - lookback - 1):
-                a = data[i:(i + lookback), 0]
-                dataX.append(a)
-                dataY.append(data[i + lookback, 0])
-            return np.array(dataX), np.array(dataY)
-        
-        lookback = 20
-        trainX, trainY = create_dataset(train_data, lookback)
-        
-        trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-        
-        st.write('Building and training the model...')
-        model = Sequential()
-        model.add(LSTM(50, input_shape=(lookback, 1)))
-        model.add(Dense(1))
-        model.compile(loss='mean_squared_error', optimizer='adam', run_eagerly=True)
-        model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-        
-        st.write('Predicting stock prices...')
-        testX, testY = create_dataset(test_data, lookback)
+# Streamlit app
+st.title("Stock Price Prediction")
 
-        if testX.ndim == 1:
-            testX = np.reshape(testX, (testX.shape[0], 1))
+# Stock search bar
+symbol = st.text_input("Enter a stock symbol (e.g., AAPL)", "AAPL")
 
-        if testX.ndim == 2:
-            testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+# Download stock data for a given symbol
+@st.cache
+def download_stock_data(symbol):
+    stock_data = yf.download(symbol, start="2010-01-01", end="2022-01-01")
+    return stock_data
 
-        predicted_prices = model.predict(testX)
-        
-        predicted_prices = scaler.inverse_transform(predicted_prices)
-        
-        st.write('Plotting the results...')
-        df['Date'] = df.index
-        df_train = df[:train_size]
-        df_test = df[train_size + lookback + 1:]
-        df_test['Predicted'] = predicted_prices
-        
-        st.line_chart(df[['Close', 'Predicted']])
-        st.write(df_test)
+stock_data = download_stock_data(symbol)
 
-if __name__ == '__main__':
-    main()
+# Preprocess stock data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(stock_data["Close"].values.reshape(-1, 1))
+train_data = scaled_data[:int(len(scaled_data)*0.8)]
+test_data = scaled_data[int(len(scaled_data)*0.8):]
+
+# Define a function to create input sequences for the CNN model
+def create_sequences(data, seq_length):
+    X = []
+    y = []
+    for i in range(seq_length, len(data)):
+        X.append(data[i-seq_length:i, 0])
+        y.append(data[i, 0])
+    return np.array(X), np.array(y)
+
+# Create input sequences for the CNN model
+seq_length = 60
+X_train, y_train = create_sequences(train_data, seq_length)
+X_test, y_test = create_sequences(test_data, seq_length)
+
+# Reshape input data to match the CNN model input shape
+X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+
+# Define the CNN model
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=3, activation="relu", input_shape=(seq_length, 1)))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(50, activation="relu"))
+model.add(Dense(1))
+
+# Compile the CNN model
+model.compile(loss="mean_squared_error", optimizer="adam")
+
+# Train the CNN model
+history = model.fit(X_train, y_train, epochs=3, batch_size=64, validation_data=(X_test, y_test), verbose=2)
+
+# Predict button
+if st.button("Predict"):
+    # Make predictions with the CNN model
+    train_predictions = model.predict(X_train)
+    test_predictions = model.predict(X_test)
+
+    # Reverse the scaling of the predictions
+    train_predictions = scaler.inverse_transform(train_predictions)
+    y_train = scaler.inverse_transform(y_train.reshape(-1, 1))
+    test_predictions = scaler.inverse_transform(test_predictions)
+    y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+    # Calculate MAE, MSE, and RMSE for the test data
+    mae = mean_absolute_error(y_test, test_predictions)
+    mse = mean_squared_error(y_test, test_predictions)
+    rmse = np.sqrt(mse)
+
+    # Plot the training and validation loss
+    st.subheader("Model Loss")
+    plt.plot(history.history["loss"])
+    plt.plot(history.history["val_loss"])
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["Train", "Test"], loc="upper right")
+    st.pyplot()
+
+    # Plot the predicted and actual prices for the test data
+    st.subheader("Actual vs. Predicted Prices")
+    plt.plot(y_test, label="Actual")
+    plt.plot(test_predictions, label="Predicted")
+    plt.legend()
+    st.pyplot()
+
+    # Print the evaluation metrics
+    st.subheader("Evaluation Metrics")
+    st.write("Mean Absolute Error:", mae)
+    st.write("Mean Squared Error:", mse)
+    st.write("Root Mean Squared Error:", rmse)
